2be5760783ff57d709e2a888246a308c7132dac5
Nguyễn Thanh Tuân <48173454+thanhtuancris@users.noreply.github.com>
2021-03-31 16:15:11 +0700

    Update connect.database.js

--- config/connect.database.js
+++ config/connect.database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect(){
     try{
-        await mongoose.connect('mongodb://GoStackTeam:HuJunk10H04ngTh4ngD3p7r41Nh4tH3M47Tr01@45.76.153.75:27017/?compressors=disabled&gssapiServiceName=mongodb', {
+        await mongoose.connect('mongodb://GoStackTeam:HuJunk10H04ngTh4ngD3p7r41Nh4tH3M47Tr01@45.76.153.75:27017/duong_data?compressors=disabled&gssapiServiceName=mongodb', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
